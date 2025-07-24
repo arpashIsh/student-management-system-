d462c215c714106e2c4c6c45500109656833f4d2
User <user@example.com>
2025-07-25 02:49:22 +0530

    Update Main_class.java to match web UI: add front.png background, four buttons with icons, and layout adjustments

--- src/student/management/system/Main_class.java
+++ src/student/management/system/Main_class.java
@@ -8,21 +8,30 @@ import javax.swing.*;
 public class Main_class extends JFrame {
     Main_class(){
 
-        ImageIcon i1 = new ImageIcon(getClass().getResource("/student/management/system/icons/home.png"));
-        Image i2 = i1.getImage().getScaledInstance(1120,630,Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel img = new JLabel(i3);
-        img.setBounds(0,0,1120,630);
-        add(img);
+        JLabel img;
+        java.net.URL imgURL = getClass().getResource("/student/management/system/icons/front.png");
+        if (imgURL != null) {
+            ImageIcon i1 = new ImageIcon(imgURL);
+            Image i2 = i1.getImage().getScaledInstance(600,400,Image.SCALE_SMOOTH);
+            ImageIcon i3 = new ImageIcon(i2);
+            img = new JLabel(i3);
+            img.setBounds(260,100,600,400);
+            add(img);
+        } else {
+            System.err.println("Background image not found");
+            img = new JLabel();
+            img.setBounds(260,100,600,400);
+            add(img);
+        }
 
         JLabel heading = new JLabel("Employee Management System");
-        heading.setBounds(340,155,400,40);
+        heading.setBounds(370,120,400,40);
         heading.setFont(new Font("Raleway",Font.BOLD,25));
         img.add(heading);
 
-        ImageIcon addIcon = new ImageIcon(getClass().getResource("/student/management/system/icons/front.png"));
+        ImageIcon addIcon = new ImageIcon(getClass().getResource("/student/management/system/icons/LoginB.png"));
         JButton add = new JButton("Add Employee", addIcon);
-        add.setBounds(335,270,180,40);
+        add.setBounds(320,200,180,40);
         add.setForeground(Color.WHITE);
         add.setBackground(Color.black);
         add.addActionListener(new ActionListener() {
@@ -34,9 +43,9 @@ public class Main_class extends JFrame {
         });
         img.add(add);
 
-        ImageIcon viewIcon = new ImageIcon(getClass().getResource("/student/management/system/icons/home.png"));
-        JButton view = new JButton("View Employee", viewIcon);
-        view.setBounds(565,270,180,40);
+        ImageIcon viewIcon = new ImageIcon(getClass().getResource("/student/management/system/icons/print.png"));
+        JButton view = new JButton("View Employees", viewIcon);
+        view.setBounds(510,200,180,40);
         view.setForeground(Color.WHITE);
         view.setBackground(Color.black);
         view.addActionListener(new ActionListener() {
@@ -48,15 +57,30 @@ public class Main_class extends JFrame {
         });
         img.add(view);
 
+        ImageIcon updateIcon = new ImageIcon(getClass().getResource("/student/management/system/icons/home.png"));
+        JButton update = new JButton("Update Employee", updateIcon);
+        update.setBounds(320,270,180,40);
+        update.setForeground(Color.WHITE);
+        update.setBackground(Color.black);
+        update.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Updatestudent();
+                setVisible(false);
+            }
+        });
+        img.add(update);
+
         ImageIcon remIcon = new ImageIcon(getClass().getResource("/student/management/system/icons/delete.png"));
         JButton rem = new JButton("Remove Employee", remIcon);
-        rem.setBounds(440,370,180,40);
+        rem.setBounds(510,270,180,40);
         rem.setForeground(Color.WHITE);
         rem.setBackground(Color.black);
         rem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 new Removestudent();
+                setVisible(false);
             }
         });
         img.add(rem);
